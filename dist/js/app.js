3950606517bf050d9f67de0eac9d9215988bba63
brianotoole <botoole518@gmail.com>
2020-02-27 20:01:55 -0500

    switch form to formcarry + zapier

--- dist/js/app.js
+++ dist/js/app.js
@@ -10834,10 +10834,10 @@ exports.default = Modal;
 "use strict";
 
 
-$(document).ready(function () {
-  $(".mc_embed_signup > form").submit(function (e) {
-    e.preventDefault(); // Prevent a new window from opening upon clicking 'Subscribe now' button
-
+$(function () {
+  $(".form").submit(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("action");
     var validForm = true; // Set initial state of valid form to true
     var inputArray = $(this).find("input.required"); // Find all required inputs and store them in array
 
@@ -10846,41 +10846,30 @@ $(document).ready(function () {
       if ($(this).val() == "") {
         validForm = false;
         $(".mc_embed_signup .error-message").show(); // if empty, show error message
-        $('.mc_embed_signup input.required').addClass('error'); // and highlight empty inputs
+        $(".mc_embed_signup input.required").addClass("error"); // and highlight empty inputs
       }
     });
 
-    // Everything checks out! Continue...
     if (validForm == true) {
       var formContainer = $(".mc_embed_signup");
-      var formData = $(this).serialize(); // Format all info and get it ready for sendoff
-
-      // AJAX magic coming up...
       $.ajax({
-        type: $(this).attr("method"),
-        url: $(this).attr("action"),
-        data: formData,
-        cache: false,
+        type: "POST",
         dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        encode: true,
-        error: function error(err) {
-          console.log("Uh, oh. There was an error:", err); // You broke something...
-        },
-        success: function success(data) {
-          console.log("Success! Here is the data:", data); // Yay!
+        url: href,
+        data: $(this).serialize(),
+        success: function success(response) {
+          if (response.status == "success") {
+            console.log("submitted");
+            $(formContainer).hide(); // Hide the initial form
+            $(".success-message").show(); // Show the checkmark
+            $("svg").addClass("active"); // Start animation of checkmark
+          } else {
+            alert("An error occured: " + response.message);
+          }
         }
-      }) // All done! Let's show the user a success message:
-      .done(function (data) {
-        $(formContainer).hide(); // Hide the initial form
-
-        $(".success-message").show(); // Show the checkmark
-        $("svg").addClass("active"); // Start animation of checkmark
       });
     }
-
-    return; // No go on form...
-  }); // end of submit function
+  });
 });
 
 /***/ })
